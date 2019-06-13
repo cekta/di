c91e844b583fa1d82219eddd4bc362aed4ada8e4
Farid Akhmedov <farid5ip50@gmail.com>
2019-06-14 02:01:40 +0700

    fix git error
    (cherry picked from commit 59c85aa612d423fa896f9e606bd1e108dcad93c3)
    
    # Conflicts:
    #	tests/Unit/Provider/AutowireTest.php
    #	tests/Unit/Provider/KeyValueTest.php

--- tests/Unit/Loader/AliasTest.php
+++ tests/Unit/Loader/AliasTest.php
@@ -6,21 +6,14 @@ namespace Cekta\DI\Test\Unit\Loader;
 use Cekta\DI\Loader\Alias;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
-use ReflectionException;
 
 class AliasTest extends TestCase
 {
-    /**
-     * @throws ReflectionException
-     */
-    public function testInvoke()
+    public function testInvoke(): void
     {
         $container = $this->createMock(ContainerInterface::class);
-        $container->method('get')
-            ->with($this->equalTo('a'))
-            ->willReturn(123);
-        $alias = new Alias('a');
         assert($container instanceof ContainerInterface);
-        $this->assertEquals(123, $alias($container));
+        $container->method('get')->with('a')->willReturn('test');
+        static::assertEquals('test', (new Alias('a'))($container));
     }
 }
