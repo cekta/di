3c01b5a39d4bc1ba0cbd14520d645d74c0d9ac66
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-11-01 22:43:25 +0500

    phpinsights integration

---
+++ phpinsights.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenNormalClasses;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Formatting\SpaceAfterNotSniff;
+use SlevomatCodingStandard\Sniffs\Classes\SuperfluousInterfaceNamingSniff;
+use SlevomatCodingStandard\Sniffs\Commenting\DocCommentSpacingSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\DisallowMixedTypeHintSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\TypeHintDeclarationSniff;
+
+return [
+    'preset' => 'default',
+    'exclude' => [
+    ],
+    'add' => [
+    ],
+    'remove' => [
+        DisallowMixedTypeHintSniff::class,
+        TypeHintDeclarationSniff::class,
+        ForbiddenNormalClasses::class,
+        SuperfluousInterfaceNamingSniff::class,
+        SpaceAfterNotSniff::class,
+    ],
+    'config' => [
+        LineLengthSniff::class => [
+            'lineLimit' => 120,
+            'absoluteLineLimit' => 0,
+            'ignoreComments' => false,
+        ],
+        DocCommentSpacingSniff::class => [
+            'linesCountBeforeFirstContent' => 0,
+            'linesCountBetweenDescriptionAndAnnotations' => 1,
+            'linesCountBetweenDifferentAnnotationsTypes' => 0,
+            'linesCountBetweenAnnotationsGroups' => 0,
+            'linesCountAfterLastContent' => 0,
+            'annotationsGroups' => [],
+        ],
+        \NunoMaduro\PhpInsights\Domain\Insights\CyclomaticComplexityIsHigh::class => [
+            'maxComplexity' => 10,
+        ],
+    ],
+];
--- src/Provider/AutowiringSimpleCache.php
+++ src/Provider/AutowiringSimpleCache.php
@@ -46,8 +46,8 @@ class AutowiringSimpleCache implements ProviderInterface
                 $this->cache->set($key, $result);
             }
             return $result;
-        } catch (InvalidArgumentException $e) {
-            throw new InvalidCacheKey($id, $e);
+        } catch (InvalidArgumentException $invalidArgumentException) {
+            throw new InvalidCacheKey($id, $invalidArgumentException);
         }
     }
 }
