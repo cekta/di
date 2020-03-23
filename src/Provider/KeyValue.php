c13180d670770b81309c65a43cf6498e509b5b8d
Evgeniy Kuvshinov <smpl@itmywork.com>
2020-03-23 06:30:41 +0500

    split keyvalue static method to provider

--- src/Provider/KeyValue.php
+++ src/Provider/KeyValue.php
@@ -4,23 +4,11 @@ declare(strict_types=1);
 
 namespace Cekta\DI\Provider;
 
-use Cekta\DI\Loader\Alias;
 use Cekta\DI\Provider\Exception\NotFound;
 use Cekta\DI\ProviderInterface;
-use InvalidArgumentException;
 
 class KeyValue implements ProviderInterface
 {
-    private const TRANSFORM = [
-        'true' => true,
-        '(true)' => true,
-        'false' => false,
-        '(false)' => false,
-        'null' => null,
-        '(null)' => null,
-        'empty' => '',
-        '(empty)' => '',
-    ];
     /**
      * @var array
      */
@@ -29,38 +17,6 @@ class KeyValue implements ProviderInterface
     public function __construct(array $values)
     {
         $this->values = $values;
-    }
-
-    public static function stringToAlias(array $values): self
-    {
-        $result = [];
-        foreach ($values as $key => $value) {
-            if (is_string($value)) {
-                $value = new Alias($value);
-            }
-            $result[$key] = $value;
-        }
-        return new static($result);
-    }
-
-    public static function stringToType(array $params): self
-    {
-        foreach ($params as $key => $value) {
-            $params[$key] = self::transform($value);
-        }
-        return new static($params);
-    }
-
-    public static function compile(string $compiledFile, callable $callable): self
-    {
-        if (!is_readable($compiledFile)) {
-            if (!is_writable(dirname($compiledFile))) {
-                throw new InvalidArgumentException("`$compiledFile` must be writable");
-            }
-            file_put_contents($compiledFile, call_user_func($callable));
-        }
-        /** @noinspection PhpIncludeInspection */
-        return new static(require $compiledFile);
     }
 
     public function provide(string $id)
@@ -74,24 +30,5 @@ class KeyValue implements ProviderInterface
     public function canProvide(string $id): bool
     {
         return array_key_exists($id, $this->values);
-    }
-
-    private static function transform($value)
-    {
-        if (is_string($value)) {
-            if (preg_match('/\A([\'"])(.*)\1\z/', $value, $matches)) {
-                $value = $matches[2];
-            }
-            $value = self::transformString($value);
-        }
-        return $value;
-    }
-
-    private static function transformString(string $value)
-    {
-        $lower = strtolower($value);
-        return array_key_exists($lower, self::TRANSFORM)
-            ? self::TRANSFORM[$lower]
-            : $value;
     }
 }
--- tests/Provider/KeyValueTest.php
+++ tests/Provider/KeyValueTest.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Cekta\DI\Test\Provider;
 
-use Cekta\DI\Loader\Alias;
 use Cekta\DI\Provider\Exception\NotFound;
 use Cekta\DI\Provider\KeyValue;
 use Cekta\DI\ProviderExceptionInterface;
 use Cekta\DI\ProviderInterface;
-use InvalidArgumentException;
-use LogicException;
 use PHPUnit\Framework\TestCase;
 
 class KeyValueTest extends TestCase
@@ -43,103 +40,5 @@ class KeyValueTest extends TestCase
     {
         $this->expectException(NotFound::class);
         (new KeyValue([]))->provide('magic');
-    }
-
-    /**
-     * @throws ProviderExceptionInterface
-     */
-    public function testStringToAlias(): void
-    {
-        $provider = KeyValue::stringToAlias(
-            [
-                'a' => 'string',
-                'b' => 123
-            ]
-        );
-        $this->assertSame(123, $provider->provide('b'));
-        $this->assertInstanceOf(Alias::class, $provider->provide('a'));
-    }
-
-    /**
-     * @throws ProviderExceptionInterface
-     */
-    public function testStringToType(): void
-    {
-        $provider = KeyValue::stringToType(
-            [
-                'a' => 123,
-                'b' => 'true',
-                'b2' => '(true)',
-                'c' => 'false',
-                'c2' => '(false)',
-                'e' => 'empty',
-                'e2' => '(empty)',
-                'n' => 'null',
-                'n2' => '(null)',
-                'r' => '"test"',
-                'r2' => "'test'",
-                'REGISTER' => 'True'
-            ]
-        );
-        $this->assertSame(123, $provider->provide('a'));
-        $this->assertTrue($provider->provide('b'));
-        $this->assertTrue($provider->provide('b2'));
-        $this->assertTrue($provider->provide('REGISTER'));
-        $this->assertFalse($provider->provide('c'));
-        $this->assertFalse($provider->provide('c2'));
-        $this->assertEmpty($provider->provide('e'));
-        $this->assertEmpty($provider->provide('e2'));
-        $this->assertNull($provider->provide('n'));
-        $this->assertNull($provider->provide('n2'));
-        $this->assertSame('test', $provider->provide('r'));
-        $this->assertSame('test', $provider->provide('r2'));
-    }
-
-    /**
-     * @throws ProviderExceptionInterface
-     */
-    public function testCompile(): string
-    {
-        $compiledFile = __DIR__ . '/compiled.php';
-        $provider = KeyValue::compile(
-            $compiledFile,
-            static function () {
-                return <<<"COMPILED"
-<?php
-
-return ['test' => 'compiled content'];
-COMPILED;
-            }
-        );
-        $this->assertSame('compiled content', $provider->provide('test'));
-        return $compiledFile;
-    }
-
-    /**
-     * @depends testCompile
-     * @param string $compiledFile
-     * @throws ProviderExceptionInterface
-     */
-    public function testCompileWithCompiledFile(string $compiledFile): void
-    {
-        $provider = KeyValue::compile(
-            $compiledFile,
-            static function () {
-                throw new LogicException("function not be called, compiled file exists");
-            }
-        );
-        $this->assertSame('compiled content', $provider->provide('test'));
-        unlink($compiledFile);
-    }
-
-    public function testCompileNotWritable(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('`/notWritable` must be writable');
-        KeyValue::compile(
-            '/notWritable',
-            static function () {
-            }
-        );
     }
 }
