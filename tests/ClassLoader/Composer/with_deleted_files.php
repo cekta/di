9560be767c165b0d3b0c57c9f040a05ebba00c8f
Evgeniy Kuvshinov <smpl@itmywork.com>
2026-01-24 03:30:52 +0500

    composer ignore not exist class

--- tests/ClassLoader/Composer/with_deleted_files.php
+++ tests/ClassLoader/Composer/with_deleted_files.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    stdClass::class => __DIR__ . '/with_deleted_files.php', // exist files must be loaded
     Iterator::class => 'file not exist', // must be ignored
+    stdClass::class => __DIR__ . '/with_deleted_files.php', // exist files must be loaded
 ];
